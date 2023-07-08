5655a3b9c6f7d6df6f6e62c055cd00006f17baac
sattar <rasoulisattar@gmail.com>
2023-07-08 23:03:21 +0330

    feat: create basic form using formik and yup

---
+++ src/components/Form/StepTwoPassAndUserForm.tsx
@@ -0,0 +1,20 @@
+import { Field, Form } from 'formik'
+import React from 'react'
+
+function StepTwoPassAndUserForm({ errors, touched }: any) {
+    return (
+        <Form>
+            <Field name="firstName" />
+            {errors.firstName && touched.firstName ? (
+                <div>{errors.firstName}</div>
+            ) : null}
+            <Field name="lastName" />
+            {errors.lastName && touched.lastName ? (
+                <div>{errors.lastName}</div>
+            ) : null}
+            <button type="submit">Submit</button>
+        </Form>
+    )
+}
+
+export default StepTwoPassAndUserForm
